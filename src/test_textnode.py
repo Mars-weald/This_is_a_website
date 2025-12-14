c6dbae46fe6d1ed0b5141a06d987f69753bbb4b2
Mars-weald <marsborleis@gmail.com>
2025-12-13 22:52:29 -0500

    textnode tests added

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_not_eq(self):
+        node1 = TextNode("This is a test", TextType.ITALIC)
+        node2 = TextNode("This is test", TextType.ITALIC)
+        self.assertNotEqual(node1, node2)
+
+    def test_type_not_eq(self):
+        node1 = TextNode("This is a test", TextType.CODE)
+        node2 = TextNode("This is a test", TextType.BOLD)
+        self.assertNotEqual(node1, node2)
+
+    def test_url_not_eq(self):
+        node1 = TextNode("This is a test", TextType.LINK, None)
+        node2 = TextNode("This is a test", TextType.LINK, "https://spoilitfortherestofus.com")
+        self.assertNotEqual(node1, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
